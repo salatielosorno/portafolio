db094078bdc8913116da0a985d89faefb9ae3b11
salatielosorno <salatiel.linares@gmail.com>
2023-05-08 20:46:34 -0600

    feat(pages/_document.tsx): add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,24 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <meta
-          name="Sofy Piñatas"
-          content="Sofy Piñatas"
-        />
+        <meta name="description" content="Todo para tu fiesta. Piñatas, Recuerdos y Decoración" />
+        <meta name="keywords" content="piñatas, fiesta, ideas para fiesta, idea de piñata, decoracion, decoración, ideas de pinatas" />
+
+        {/* <!--  Essential META Tags --> */}
+        <meta property="og:title" content="Todo para tu fiesta. Piñatas, Recuerdos y Decoración" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="http://sofypiñatas.com/sofy/images/sofy-pinatas.png" />
+        <meta property="og:url" content="http://sofypiñatas.com" />
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+
+        {/* <!--  Non-Essential, But Recommended --> */}
+        <meta property="og:description" content="Todo para tu fiesta. Piñatas, Recuerdos y Decoración" />
+        <meta property="og:site_name" content="Piñatas Sofy" />
+        <meta name="twitter:image:alt" content="Todo para tu fiesta. Piñatas, Recuerdos y Decoración" />
+
+        {/* <!--  Non-Essential, But Required for Analytics --> */}
+        {/* <meta property="fb:app_id" content="your_app_id" />
+        <meta name="twitter:site" content="@website-username" /> */}
       </Head>
       <body>
         <Main />
