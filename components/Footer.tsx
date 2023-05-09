d03b3e190f5dc74098d618c12d778c6fd36bb746
salatielosorno <salatiel.linares@gmail.com>
2023-05-08 20:31:19 -0600

    chore(components/footer.tsx): add terms and conditions

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,35 @@
 export default function Footer() {
   return (
-    <footer className="center mt-5 flex justify-center space-x-4 bg-spurple p-4 text-white text-xs" style={{ 'position': 'relative' }}>
-      <p>Sofy Piñatas </p>
-      <span>|</span>
-      <span
-
-        className="font-medium text-white"
-      >
-        2023
-      </span>
-      <div className="chat-support__icons">
-        <a
-          className="chat-support__icon block mb-2"
-          href="https://api.whatsapp.com/send/?phone=5212251121397"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="/sofy/images/whats-icon.png" alt="whats-app-icon" />
-        </a>
-        {/* <div className="chat-support__icon"></div> */}
+    <footer className="mt-5 bg-spurple p-4 text-white text-xs" style={{ 'position': 'relative' }}>
+      <div className="center flex justify-center space-x-4">
+        <p>* Los precios no incluye costo por envío. Pedidos con mínimo 5 días de anticipación al evento. Todos los pedidos pasan a lista de espera con el 50% de anticipo.</p>
       </div>
+      <br/>
+      <hr/>
+      <br/>
+      <div className="center flex justify-center space-x-4"><p>Sofy Piñatas </p>
+        <span>|</span>
+        <span
 
+          className="font-medium text-white"
+        >
+          2023
+        </span>
+        <div className="chat-support__icons">
+          <a
+            className="chat-support__icon block mb-2"
+            href="https://api.whatsapp.com/send/?phone=5212251121397"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/sofy/images/whats-icon.png" alt="whats-app-icon" />
+          </a>
+          {/* <div className="chat-support__icon"></div> */}
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
     </footer>
   )
 }
