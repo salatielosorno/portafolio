7be45cce7c211e6900e54c886bc887798b241347
salatielosorno <salatiel.linares@gmail.com>
2023-05-10 13:42:14 -0600

    feat(pages/_app.tsx): add google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Footer from '../components/Footer'
 import Head from 'next/head'
 import { Montserrat, Quicksand } from 'next/font/google'
+import Script from 'next/script'
 
 const quicksand = Quicksand({
   subsets: ['latin'],
@@ -14,9 +15,23 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Sofy Piñatas</title>
       </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-412BS5HS4T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-412BS5HS4T');
+        `}
+      </Script>
       <main className={`${quicksand.variable} font-sans`}>
         <Component {...pageProps} />
-        <div className='chat-support__icon'/>
+        <div className='chat-support__icon' />
         <Footer />
       </main>
     </>
