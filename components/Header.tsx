701fb4eb100e25895bf7af97a60771068e06c0d8
salatielosorno <salatiel.linares@gmail.com>
2023-08-19 00:02:37 -0600

    feat(website): redesign home section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,42 +1,50 @@
 import Image from 'next/image'
-import img from '../public/images/sofy-pinatas-background.png'
-import logo from '../public/images/sofy-pignatas.jpeg';
+import img from '../public/images/confeti.png'
+import logo from '../public/images/sofy-pinatas-2024.png';
+import wave from '../public/images/wave.png'
 
 export default function Header({ scrollHandler }) {
   return (
     <header className="relative">
-      <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+      <div>
+        <ul>
+          <li><a href='#'>Piñatas</a></li>
+          <li><a href='#'>Piñatas</a></li>
+          <li><a href='#'>Piñatas</a></li>
+        </ul>
+      </div>
+      <div className="absolute inset-x-0 bottom-0 h-1/2" />
       <div className="mx-auto">
-        <div className="relative shadow-xl sm:overflow-hidden">
-          <div className="absolute inset-0">
+        <div className="relative shadow-xl sm:overflow-hidden h-screen">
+          <div className="relative px-4 pt-16 sm:pb-6 sm:pt-24 lg:pt-32 lg:px-8 flex justify-center	">
+            <Image id='logo' width={800} src={logo} alt="Sofy Piñatas" className='h-2/4 lg:w-3/5 xl:w-2/5' />
+          </div>
+          <div className='md:hidden'>
+            <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-5xl  font-semibold tracking-wide text-orange-600">
+              <span className="block text-white legend--text-shadow">La vida es mejor con una piñata.</span>
+              {/* <span className="block text-white font-medium legend--text-shadow"></span> */}
+            </p>
+          </div>
+          <div>
             <Image
               priority
               fill
-              className="h-full w-full object-cover"
+              className="confeti"
               src={img}
               placeholder="blur"
               alt="Sofy Piñatas"
             />
-            
-          </div>
-          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-            {/* <h1 className="mt-1 text-center font-bold text-gray-800 sm:tracking-tight text-7xl lg:text-9xl">
-              <span className='font-bold text-spink block magz magz-first'>Sofy</span> <span className='text-syellow block magz-no magz-second'>Piñatas</span>
-            </h1> */}
-            <Image id='logo' width={800} src={logo} alt="Sofy Piñatas"/>
-            <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl  font-semibold tracking-wide text-orange-600">
-              <span className="block text-white legend--text-shadow">La vida es mejor con una</span>
-              <span className="block text-white font-medium legend--text-shadow">piñata.</span>
-            </p>
 
-           {/*  <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
-              <button
-                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8"
-                onClick={scrollHandler}
-              >
-                Shop piñata
-              </button>
-            </div> */}
+          </div>
+          <div>
+            <Image
+              priority
+              fill
+              className="wave"
+              src={wave}
+              placeholder="blur"
+              alt="Sofy Piñatas"
+            />
           </div>
         </div>
       </div>
